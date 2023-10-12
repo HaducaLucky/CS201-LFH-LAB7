f71ce533f43fcc1bd0cf1a7bcb1dfe5a83e0b229
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2023-10-12 17:08:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = 'p@ssword2023';
+
+if ( password.length >= 8 &&  password.includes("p@ssword")){
+        console.log("Valid password");
+} else{
+    console.log("Invalid password");
+}
